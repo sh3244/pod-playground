c79de961791c8062e9b2affdcf529bd8500e4be0
Sam Huang <sh3244@nyu.edu>
2019-02-20 16:10:23 -0500

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Sam Huang on 2/20/19.
+//  Copyright © 2019 Sam Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
